d10c5d9b879e170f76ee3772c19db1cf231dd0e4
sungnga <708@holbertonschool.com>
2019-08-14 02:59:47 +0000

    Update class attributes for Review

--- models/review.py
+++ models/review.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """This is the review class"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, Integer, Float, ForeignKey
 
-
-class Review(BaseModel):
+class Review(BaseModel, Base):
     """This is the class for Review
     Attributes:
         place_id: place id
