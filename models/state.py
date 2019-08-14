adbdbef6be9c874317f375866ae89e8c2629ce90
David Kwan <dwkwan1989@gmail.com>
2019-08-14 00:57:15 +0000

    Add task 6

--- models/state.py
+++ models/state.py
@@ -1,11 +1,23 @@
 #!/usr/bin/python3
 """This is the state class"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """This is the class for State
     Attributes:
         name: input name
     """
-    name = ""
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state")
+
+    @property
+    def cities(self):
+        citylist = []
+        for city in self.cities:
+            if city.state_id == self.id:
+                citylist += city
+        return citylist
