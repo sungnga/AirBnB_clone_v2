39ce38298120a811b0da940ab6a2c5d5d26d97a5
David Kwan <dwkwan1989@gmail.com>
2019-08-15 03:06:05 +0000

    Add test file

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -5,15 +5,13 @@
 class TestFileStorage(unittest.TestCase):
     '''this will test the FileStorage'''
 
-    @classmethod
-    def setUpClass(cls):
-        pass
-
-    @classmethod
-    def teardown(cls):
+    def setUp(cls):
         pass
 
     def tearDown(self):
+        pass
+
+    def test_func(self):
         pass
 
 if __name__ == "__main__":
