de8922ca920c4a61f09764a7c4b2060ff0ff2ee7
sungnga <708@holbertonschool.com>
2019-09-01 01:24:41 +0000

    Add additional blank lines

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -9,22 +9,27 @@ app = Flask(__name__)
 def hello():
     return 'Hello HBNB!'
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hello_hbnb():
     return 'HBNB'
 
+
 @app.route('/c/<text>', strict_slashes=False)
 def hello_c(text):
     return 'C %s' % text.replace("_", " ")
+
 
 @app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python_text(text="is cool"):
     return 'Python {}'.format(text.replace("_", " "))
 
+
 @app.route('/number/<int:n>', strict_slashes=False)
 def number(n):
     return '%d is a number' % n
+
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def num_temp(n):
