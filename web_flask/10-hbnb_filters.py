547223223c00fc761166c7ce3575e097fa618432
sungnga <708@holbertonschool.com>
2019-09-05 08:08:40 +0000

    Import State and Amenity models

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -1,20 +1,9 @@
 #!/usr/bin/python3
 """A script that starts a Flask web application"""
 
-from models import storage
+from models import storage, State, Amenity
 from flask import Flask, request, render_template
 app = Flask(__name__)
-
-
-@app.route('/states', strict_slashes=False)
-@app.route('/states/<id>', strict_slashes=False)
-def cities_list(id=None):
-    states = storage.all('State')
-    cities = storage.all('City')
-    if id:
-        id = '{}.{}'.format('State', id)
-    return render_template('9-states.html',
-                           states=states, id=id)
 
 
 @app.route('/hbnb_filters')
